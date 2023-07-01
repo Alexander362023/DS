94d3cfa318039b2cebafac4ffb86b898aae15bbd
Alexander <sahaal2019@mail.ru>
2023-07-01 22:43:47 +0400

    Initial3

--- Script-1.sql
+++ Script-1.sql
@@ -4,8 +4,7 @@ create table if not exists Жанр(
 );
 
 create table if not exists Исполнители(
-	Исполнители_id SERIAL PRIMARY key,
-	Альбом VARCHAR(20) UNIQUE NOT null,
+	Исполнители_id SERIAL PRIMARY key,	
 	Имя varchar(20) NOT null	
 );
 
@@ -14,20 +13,12 @@ create table if not exists Исполнители_и_Жанр(
 	Исполнители integer references Исполнители(Исполнители_id),
 	constraint pk primary key(Жанр, Исполнители)
 );
-create table if not exists Треки(
-	Треки_id SERIAL primary key,
-	Название VARCHAR(50) UNIQUE NOT null,
-	Длительность numeric(15) NOT null,
-	Альбом VARCHAR(50) UNIQUE NOT null
-);	
 
 create table if not exists Альбом(
 	Альбом_id SERIAL primary key,
-	Треки integer references Треки(Треки_id),
 	Название VARCHAR(50) UNIQUE NOT null,
-	release date	
+	release date check(release>1980)
 );
-
 
 create table if not exists Исполнители_и_Альбом(
 	Альбом integer references Альбом(Альбом_id),
@@ -35,10 +26,17 @@ create table if not exists Исполнители_и_Альбом(
 	constraint pk1 primary key(Альбом, Исполнитель)
 );
 
+create table if not exists Треки(
+	Треки_id SERIAL primary key,
+	Альбом integer references Альбом(Альбом_id), 
+	Название VARCHAR(50) UNIQUE NOT null,
+	Длительность numeric(10,5) check(Длительность>2) NOT null
+);	
+
 create table if not exists Сборник(
 	Сборник_id SERIAL primary key,
 	Название VARCHAR(50) UNIQUE NOT null,
-	release date
+	release date check(release>1980)
 );
 
 create table if not exists Сборник_и_Треки(
@@ -46,4 +44,6 @@ create table if not exists Сборник_и_Треки(
 	Треки integer references Треки(Треки_id),
 	constraint pk2 primary key(Сборник, Треки)
 );
+
+
 
